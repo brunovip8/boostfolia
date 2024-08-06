fe2fcb1549e932194efbaba9da5bae7f1452e0da
brunovip8 <143424181+brunovip8@users.noreply.github.com>
2024-08-06 16:02:23 +0300

    l6

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -2,6 +2,28 @@ $(document).ready(function () {
     $(".bxslider").bxSlider({
         pager: false,
     })
+
+    function setEqualHeight(selector) {
+        let maxHeight = 0;
+        $(selector).each(function() {
+            console.log($(this).height);
+            if($(this).height()>maxHeight) {
+                maxHeight = $(this).height()
+            }
+        })
+
+    }
+    $(selector).height(maxHeight)
+
+    $(".bxslider__team").bxSlider({
+        controls: false,
+        onSliderLoad: function () {
+            setEqualHeigh("ul.bxslider__team > li >div.teamslider")
+        },
+        onSliderAfter: function () {
+            setEqualHeigh("ul.bxslider__team > li >div.teamslider")
+        }
+    })
 })
 
 $(".menu").on("click", function () {
